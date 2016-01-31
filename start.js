84c5c24aa9f49deeb69c0c371e382cfade6482dd
BingoLee <bingohlee@gmail.com>
2016-01-31 17:59:52 +0800

    修复服务器重启后，前端若有未退出socket重连导致的服务读取房间信息异常

--- start.js
+++ start.js
@@ -75,17 +75,18 @@ io.on('connection', function(socket){
      
     //监听用户退出
     socket.on('disconnect', function(){
-    	if(onlineRooms[socket.rid].users[socket.uid]) {
+
+    	if(onlineRooms[socket.rid] && onlineRooms[socket.rid].users[socket.uid]) {
     		delete onlineRooms[socket.rid].users[socket.uid];
 	    	onlineRooms[socket.rid].userCount--;
-
-	    	var msg = {
-	    		uid: socket.uid,
-	    		rid: socket.rid,
-	    		nickname: socket.nickname
-	    	};
-        	io.emit("logout"+socket.rid, msg);
     	}
+
+    	var msg = {
+    		uid: socket.uid,
+    		rid: socket.rid,
+    		nickname: socket.nickname
+    	};
+    	io.emit("logout"+socket.rid, msg);
     });
 
     socket.on("logoutRoom", function() {
